2cf04f905be0526f0b994ca7ae05bbfbb85ef31f
cypherHades <xjadezbix@gmx.com>
2024-01-09 11:18:18 -0500

    Config files

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      title: "Restaurant Project",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  optimization: {
+    // runtimeChunk: "single",
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname + 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+}
